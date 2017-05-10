ce6ae2b25673f4b5012077d803a07188f94f20bb
Andreas Östlund <aost@Andreass-MacBook-Pro.local>
2017-05-10 20:27:04 +0200

    added encryption to usermodel

--- models/user.js
+++ models/user.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = mongoose.Schema(
-    {
-        id: Number,
-        username: String,
-        password: String,
-        firstname: String,
-        lastname: String
-    }
-);
+const UserSchema = mongoose.Schema({
+    id: Number,
+    username: String,
+    password: String,
+    firstname: String,
+    lastname: String
+});
 
-module.exports = mongoose.model('user', userSchema)
+UserSchema.pre('save', next => {
+    const user = this;
+    if (!user.isModified('password')) return next()
+    bcrypt.genSalt((error, salt) => {
+        if (error) return next(error)
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error)
+
+            user.password = hash;
+            next()
+        })
+    })
+});
+
+UserSchema.methods.checkPassword = (password, callback) => {
+    bcrypt.compare(password, this.password, (error, match) => {
+        if (error) return callback(error)
+        callback(null, match);
+    });
+}
+
+module.exports = mongoose.model('User', UserSchema);
